c60925211c5f8eddeb5b06a54512dc1d31b02c44
vasusharmaiiicm <vasu.sharma@iiicm.com>
2025-08-28 17:55:30 -0400

    Add files via upload

---
+++ Main-IG Email.py
@@ -0,0 +1,25 @@
+import asyncio
+from nbconvert import NotebookExporter
+from nbconvert.preprocessors import ExecutePreprocessor
+from nbformat import read, write
+
+print("setting policy")
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+input_ntbk = "J:\\7A. IG-Main Email\\v2.ipynb"
+output_ntbk = "J:\\7A. IG-Main Email\\v2.ipynb"
+
+print("loading ntbk")
+
+with open(input_ntbk, "r", encoding="utf-8") as f:
+	notebook = read(f, as_version=4)
+
+print("executing ntbk")
+executor = ExecutePreprocessor(timeout=600, kernel_name="python3")
+executor.preprocess(notebook, {'metadata': {'path': "J:\\7A. IG-Main Email\\"}})
+
+print("saving ntbk")
+with open(output_ntbk, "w", encoding="utf-8") as f:
+	write(notebook,f)
+
+print("Done")
